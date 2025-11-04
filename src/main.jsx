25603466b4dd84873cef8347f4e053bd20062c69
ArsenGavriluk13 <havryliuk.arsen@student.uzhnu.edu.ua>
2025-11-04 11:15:07 +0200

    Lab5

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Імпортує App.jsx
+import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; // Імпорт BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       {' '}
-      {/* <-- Важливо: App обгорнуто в BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
