12d48b81465b2b1a6123adae7a551f5e1d5a2308
ArsenGavriluk13 <havryliuk.arsen@student.uzhnu.edu.ua>
2025-11-09 22:52:26 +0200

    Lab5

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,8 @@
-// src/pages/ProfilePage.jsx
-
-// Цей компонент не потребує 'React' у нових версіях
 import Header from '../components/Header';
 import { Link } from 'react-router-dom';
 
 const ProfilePage = () => {
   return (
-    // Використаємо стилі з HomePage для загального контейнера
     <div style={{ padding: '0 15px', maxWidth: '1200px', margin: '0 auto' }}>
       <Header />
       <main style={{ padding: '40px 0' }}>
